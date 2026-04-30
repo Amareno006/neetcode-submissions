8619cd2347df2f1c389b3a2cf83adbfe60abb1b3
Amar Banerjee <79227579+Amareno006@users.noreply.github.com>
2026-04-29 22:32:55 -0400

    Add: string-encode-and-decode - submission-35

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-35.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        if len(strs) == 0: 
+            return ""
+        s=""
+        for char in strs:  
+            s += (f"{len(char)},")
+
+        s += "'#'"
+        for word in strs:
+            s += (word)
+        
+
+        return s
+
+    def decode(self, s: str) -> List[str]:
+
+        if s == "":
+            return []
+        x = s.split("'#'")
+        dummy = x[0].split(",")
+        word_array = []
+        starting = 0
+        for ifg in dummy: 
+            if ifg.isdigit():
+                word_array.append(x[1][starting: starting + int(ifg)])
+                starting += int(ifg)
+
+        return word_array
