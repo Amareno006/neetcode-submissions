0df4f3c2b685b41f88ab1d9a9af1a6281c2c10c5
Amar Banerjee <79227579+Amareno006@users.noreply.github.com>
2026-05-04 21:24:30 -0400

    Add: valid-sudoku - submission-16

---
+++ Data Structures & Algorithms/valid-sudoku/submission-16.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        valid_row = { i: {} for i in range(9)}
+        valid_column = {i: {} for i in range(9)}
+        valid_box = {(i, j): {} for i in range(3) for j in range(3)}
+        for index, row in enumerate(board):
+            for column, char in enumerate(row):
+                box = (index // 3, column // 3)
+                if char in valid_row[index] or char in valid_column[column] or char in valid_box[box]:
+                    return False
+                if char != ".":
+                    valid_row[index][char] = 1
+                    valid_column[column][char] = 1
+                    valid_box[box][char] = 1
+
+        return True
