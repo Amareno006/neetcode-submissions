ff9cd3898f0a129bfb4b725d586f514a6cb363a6
Amar Banerjee <79227579+Amareno006@users.noreply.github.com>
2026-06-12 21:25:36 -0400

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed))                         
+        times = [(target-p)/s for p, s in cars]
+        fleet = 0
+        state = 0
+        for t in reversed(range(len(times))):
+            if state < times[t]:
+                fleet += 1
+                state = times[t]
+
+        return fleet
